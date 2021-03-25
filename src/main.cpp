7e2f240f853e4882cd0572142374319c373f5ee1
Kohei Kumazaki <kumazaki98@gmail.com>
2021-03-26 03:30:51 +0900

    fix bug

--- src/main.cpp
+++ src/main.cpp
@@ -5,43 +5,43 @@
 std::vector<Segment> segments =  {
 
 	// Border
-	{{   0,   0}, { 1280,   0}},
-	{{ 1280,   0}, { 1280, 720}},
-	{{ 1280, 720}, {   0, 720}},
-	{{   0, 720}, {   0,   0}},
+	{{0,0}, {1280,0}},
+	{{1280,0}, {1280,720}},
+	{{1280,720}, {0,720}},
+	{{0,720}, {0,0}},
 
 	// Polygon #1
-	{{100,150}, {120,50}},
-	{{120,50}, {200,80}},
-	{{200,80}, {140,210}},
-	{{140,210}, {100,150}},
+	{{200,300}, {240,100}},
+	{{240,100}, {400,160}},
+	{{400,160}, {280,420}},
+	{{280,420}, {200,300}},
 
 	// Polygon #2
-	{{100,200}, {120,250}},
-	{{120,250}, {60,300}},
-	{{60,300}, {100,200}},
+	{{200,400}, {240,500}},
+	{{240,500}, {120,600}},
+	{{120,600}, {200,400}},
 
 	// Polygon #3
-	{{200,260}, {220,150}},
-	{{220,150}, {300,200}},
-	{{300,200}, {350,320}},
-	{{350,320}, {200,260}},
+	{{400,520}, {440,300}},
+	{{440,300}, {600,400}},
+	{{600,400}, {700,640}},
+	{{700,640}, {400,520}},
 
 	// Polygon #4
-	{{340,60}, {360,40}},
-	{{360,40}, {370,70}},
-	{{370,70}, {340,60}},
+	{{680,120}, {720,80}},
+	{{720,80}, {740,140}},
+	{{740,140}, {680,120}},
 
 	// Polygon #5
-	{{450,190}, {560,170}},
-	{{560,170}, {540,270}},
-	{{540,270}, {430,290}},
-	{{430,290}, {450,190}},
+	{{900,360}, {1020,340}},
+	{{1020,340}, {1080,540}},
+	{{1080,540}, {860,680}},
+	{{860,680}, {900,360}},
 
 	// Polygon #6
-	{{400,95}, {580,50}},
-	{{580,50}, {480,150}},
-	{{480,150}, {400,95}}
+	{{800,190}, {1160,100}},
+	{{1160,100}, {960,300}},
+	{{960,300}, {800,190}}
 
 };
 
@@ -49,15 +49,15 @@ int main(int argc, char const *argv[]) {
 
 	int w = 1280;
 	int h = 720;
-	std::string title = "Astar demo";
+	std::string title = "Ratcasting demo";
 	initImgui(w, h, title);
 
 	// Our state
 	bool show_demo_window = true;
 	bool show_another_window = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-	ImVec4 seg_color = ImVec4(1.0f, 1.0f, 1.0f, 1.00f);
-	ImVec4 ray_color = ImVec4(1.0f, 0.0f, 0.0f, 1.00f);
+	ImVec4 seg_color = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);
+	ImVec4 ray_color = ImVec4(1.0f, 1.0f, 1.0f, 1.00f);
 
 	// Main loop
 	while (!glfwWindowShouldClose(window))
@@ -81,7 +81,6 @@ int main(int argc, char const *argv[]) {
 			Ray ray;
 			ray.pos.x = mouse_pos.x;
 			ray.pos.y = mouse_pos.y;
-			ray.length = 100.0;
 			ImGuiWindowFlags window_flags = 0;
 			window_flags |= ImGuiWindowFlags_NoMove;
 			window_flags |= ImGuiWindowFlags_NoResize;
@@ -94,7 +93,12 @@ int main(int argc, char const *argv[]) {
 			ImGui::Begin("Ray Casting", nullptr, window_flags);
 			std::vector<Vector2D> intersections;
 
-			for (double angle = 0; angle < M_PI * 2; angle += (M_PI * 2)/ 50) {
+			static int num_ray = 50;
+			static int len_ray = 100;
+			static bool show_obstacle = true;
+			ray.length = double(len_ray);
+
+			for (double angle = 0; angle < M_PI * 2; angle += (M_PI * 2)/double(num_ray) ) {
 				ray.dir = angle;
 				Vector2D closest_intersection;
 				double closest = __DBL_MAX__;
@@ -120,20 +124,32 @@ int main(int argc, char const *argv[]) {
 			}
 
 			ImDrawList *draw_list = ImGui::GetWindowDrawList();
-			for (size_t i = 0; i < segments.size(); i++) {
-				ImVec2 start = {(float)segments[i].begin.x, (float)segments[i].begin.y};
-				ImVec2 end = {(float)segments[i].end.x, (float)segments[i].end.y};
-				draw_list->AddLine(start, end, ImColor(seg_color), 2.0);
+			if (show_obstacle) {
+				for (size_t i = 0; i < segments.size(); i++) {
+					ImVec2 start = {(float)segments[i].begin.x, (float)segments[i].begin.y};
+					ImVec2 end = {(float)segments[i].end.x, (float)segments[i].end.y};
+					draw_list->AddLine(start, end, ImColor(seg_color), 2.0);
+				}
 			}
 
-			ImGui::Text("Intersection num: %lu", intersections.size());
-			for (size_t i = 0; i < intersections.size(); i++)
-			{
+			for (size_t i = 0; i < intersections.size(); i++) {
 				ImVec2 start = mouse_pos;
 				ImVec2 end = {(float)intersections[i].x, (float)intersections[i].y};
 				draw_list->AddLine(start, end, ImColor(ray_color), 2.0);
+
+				double dx = end.x - start.x;
+				double dy = end.y - start.y;
+				double d = dx*dx+dy*dy;
+				if (d < ray.length*ray.length*0.99)
+					draw_list->AddCircleFilled(end, 3.0, ImColor(ray_color));
 				// ImGui::Text("(%f, %f)", end.x, end.y);
 			}
+
+			ImGui::PushItemWidth(400);
+			ImGui::SliderInt("Num of rays", &num_ray, 1, 360);
+			ImGui::SliderInt("Len of rays", &len_ray, 100, 2000);
+			ImGui::PopItemWidth();
+			ImGui::Checkbox("Show Obstacless", &show_obstacle);
 
 			ImGui::End();
 		}
--- src/raycasting.cpp
+++ src/raycasting.cpp
@@ -12,12 +12,19 @@ bool getIntersection(const Ray &ray, const Segment &segment, Vector2D &intersect
   seg_delta.y = segment.end.y - segment.begin.y;
 
   double T2 = (ray_delta.x * (segment.begin.y - ray.pos.y) + ray_delta.y * (ray.pos.x - segment.begin.x)) / (seg_delta.x * ray_delta.y - seg_delta.y * ray_delta.x);
-  double T1 = (segment.begin.x + seg_delta.x * T2 - ray.pos.x) / ray_delta.x;
+  double T1 = 0.0;
+  
+  if ( (M_PI_4 <= ray.dir && ray.dir <= M_PI_4 * 3.0) || (M_PI_4 * 5 <= ray.dir && ray.dir <= M_PI_4 * 7.0) ) {
+    T1 = (segment.begin.y + seg_delta.y * T2- ray.pos.y) / ray_delta.y;
+  }
+  else {
+    T1 = (segment.begin.x + seg_delta.x * T2 - ray.pos.x) / ray_delta.x;
+  }
 
-  if (T1 < 0)
+  if (T1 < 0.0)
     return false;
 
-  if (T2 < 0 || T2 > 1)
+  if (T2 < 0.0 || T2 > 1.0)
     return false;
 
   intersection.x = ray.pos.x + ray_delta.x * std::min(1.0, T1);
